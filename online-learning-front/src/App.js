52932cd062e1430b2fccc9f15ad2160e8e98cfca
Nzioka6350 <jamesnziokamutua1@gmail.com>
2025-08-20 11:55:39 +0300

    Frontend set up

--- online-learning-front/src/App.js
+++ online-learning-front/src/App.js
@@ -1,12 +1,19 @@
-import './App.css';
-import LandingPage from './components/LandingPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <LandingPage/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
